62326e14522f680e506ae1c475a9e586920ff9c8
mdabdulhalim <htmlcssjs2023>
2023-03-24 21:51:30 +0600

    Create Component and return it

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import './custom.css';
 
-const name = 'Abdul Halim';
+const number = 28373;
 const singer = {name: 'Jihad', job:'Nai', address:'B.Baria'};
 const singer2 = {name:'Jihad Hasan', job: 'IT Engineer'};
 
@@ -14,31 +14,23 @@ const singerStyle = {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <div className="container">
-          <h3>Hello Dude React ! {name}</h3>
-          <div className="music">
-              <p>{singer.name} {singer.job} {singer.address}</p>
-              <p>{singer2.name} {singer2.job} {singer2.address}</p>
-              <h1 style={singerStyle}>Smell code</h1>
-          </div>
-        </div>
-        <p>
-           <code>Welcome to  React</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Zoomit></Zoomit>
+     <Zoomit></Zoomit>
+     <Zoomit></Zoomit>
+       
     </div>
   );
 }
 
+function Zoomit(){
+  return (
+   
+    <div className='zoomit'>
+       <h3>Employee </h3>
+        <p><strong>Employee Id :</strong> {number} </p>
+        <p><strong>Employee Name :</strong> Md Abdul Halim </p>
+        <p><strong>Employee Designaiton :</strong> Software Engineer </p>
+    </div>
+  );
+}
 export default App;
